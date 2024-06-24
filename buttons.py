0633267f08d001c9e045da97117afc4315f9ccfb
Javier Luque Sáiz <167310505+JavierLuqueSaiz@users.noreply.github.com>
2024-06-24 14:04:50 +0200

    Add files via upload

---
+++ buttons.py
@@ -0,0 +1,29 @@
+import streamlit as st  
+
+def compra_venta():
+    # Definir las opciones de selección
+    option = st.sidebar.radio(
+        label="What are you looking for?:",
+        options=["Buy", "Rent"],
+    )
+
+    return option
+
+def fotocasa_idealista(option):
+    options = ["Idealista", "Fotocasa"]
+    if option == "Buy":
+        pagina = st.sidebar.multiselect(
+            label="What pages do you want to search?",
+            options=options,
+            default=options
+        )
+    else:
+        pagina = st.sidebar.radio(
+            label="What pages do you want to search?",
+            options=options
+        )
+    return pagina
+
+def metros_cuadrados():
+    m2 = st.sidebar.number_input("Selecciona un número", min_value=0, max_value=500, value=90, step=1)
+    return m2
